801d3e692ef73a1b34cf62eebd4620483dd6d9dd
Rajmund Waszkiel <rajmund.wu@gmail.com>
2018-12-05 10:14:26 +0100

    Updates

---
+++ precache-manifest.7b438ff362ed475f30d6fe219262d9ae.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "345e184c86de4c34f657b23d3492d76a",
+    "url": "/unpackEventPage/static/media/contact_background.345e184c.png"
+  },
+  {
+    "revision": "f9aafe91c72e1cc05165",
+    "url": "/unpackEventPage/static/css/main.964c1948.chunk.css"
+  },
+  {
+    "revision": "4e564b5067d37734ed8e",
+    "url": "/unpackEventPage/static/js/1.4e564b50.chunk.js"
+  },
+  {
+    "revision": "15d7e7a7b754c69a5e66",
+    "url": "/unpackEventPage/static/js/runtime~main.15d7e7a7.js"
+  },
+  {
+    "revision": "dde2a58a135db21e44514602feb68510",
+    "url": "/unpackEventPage/static/media/rise_body.dde2a58a.png"
+  },
+  {
+    "revision": "74e9069ded4e1d2b70098dfb39869bed",
+    "url": "/unpackEventPage/static/media/sansara_group.74e9069d.png"
+  },
+  {
+    "revision": "5d58591d7a3f39da6d2c03dc91cbe6a4",
+    "url": "/unpackEventPage/static/media/work_comfort.5d58591d.png"
+  },
+  {
+    "revision": "981b2badd688224dcae3755dcab93acf",
+    "url": "/unpackEventPage/static/media/save.981b2bad.png"
+  },
+  {
+    "revision": "4a9a35563637dcdf2c669382856f88fa",
+    "url": "/unpackEventPage/static/media/recognition.4a9a3556.png"
+  },
+  {
+    "revision": "15448cc78531d64fc716dda482cd11a0",
+    "url": "/unpackEventPage/static/media/countdown.15448cc7.png"
+  },
+  {
+    "revision": "46c6d5ac11e8b27afb21259b1d70d16e",
+    "url": "/unpackEventPage/static/media/enthusiasm.46c6d5ac.png"
+  },
+  {
+    "revision": "d77a62c3a3defab821da182a370aa92a",
+    "url": "/unpackEventPage/static/media/succeed.d77a62c3.png"
+  },
+  {
+    "revision": "f9aafe91c72e1cc05165",
+    "url": "/unpackEventPage/static/js/main.f9aafe91.chunk.js"
+  },
+  {
+    "revision": "e1eda469c11e0da5abfa59156c8031d2",
+    "url": "/unpackEventPage/static/media/offer_background.e1eda469.png"
+  },
+  {
+    "revision": "9ca7f9d41f3654889eb0279e9620d34e",
+    "url": "/unpackEventPage/static/media/down_box.9ca7f9d4.png"
+  },
+  {
+    "revision": "8387d00118a01c14dfe47f66d288b766",
+    "url": "/unpackEventPage/static/media/box2.8387d001.png"
+  },
+  {
+    "revision": "f0a014ad36b97a27923592611d984486",
+    "url": "/unpackEventPage/static/media/bubbles-tablet.f0a014ad.png"
+  },
+  {
+    "revision": "c583ad4c4912c8102fff4027d016529d",
+    "url": "/unpackEventPage/static/media/background.c583ad4c.png"
+  },
+  {
+    "revision": "efc13bb9d5b2429a746eb8d729bbe580",
+    "url": "/unpackEventPage/static/media/fontello.efc13bb9.eot"
+  },
+  {
+    "revision": "0019a4e4ef68a2d0577b0c351c90b8c5",
+    "url": "/unpackEventPage/static/media/fontello.0019a4e4.woff2"
+  },
+  {
+    "revision": "86f0d601dfaea3e4f37edce8cf31c932",
+    "url": "/unpackEventPage/static/media/fontello.86f0d601.ttf"
+  },
+  {
+    "revision": "d7790aa86811b52e0a77e624c1578d92",
+    "url": "/unpackEventPage/static/media/fontello.d7790aa8.woff"
+  },
+  {
+    "revision": "d842d5dcb65e9cbf8fa0c3711b88a388",
+    "url": "/unpackEventPage/static/media/fontello.d842d5dc.svg"
+  },
+  {
+    "revision": "9e4e6d67d01eac8fa27ceca7f8b7ef7f",
+    "url": "/unpackEventPage/index.html"
+  }
+];
--- src/Functions/showFunction.js
+++
@@ -1,8 +0,0 @@
-const showFunction = (element) =>{
-    if(window.innerHeight+window.scrollY>element.offsetTop){
-        if(!element.classList.contains('show-component')){
-            element.classList.add('show-component')
-        }
-    }
-}
-export default showFunction
